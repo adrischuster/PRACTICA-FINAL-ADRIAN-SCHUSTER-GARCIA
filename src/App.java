26798f273fdecbe0827ed68a13287735f1f0cc0d
adrischuster <adrischuster@usal.es>
2025-12-06 20:59:41 +0100

    CRUD1

--- src/App.java
+++ src/App.java
@@ -1,3 +1,33 @@
-public class Ap {
+import controller.Controller;
+import model.BinaryRepository;
+import model.Model;
+import model.QuestionBackupIO;
+import model.IRepository;
+import model.JSONQuestionBackupIO;
+import java.util.ArrayList;
+import model.QuestionCreator;
+import view.BaseView;
+import view.InteractiveView;
 
+// PROBLEMA GORDO CON GITHUB EN APP.JAVA
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Settear
+        IRepository repository = new BinaryRepository();
+        QuestionBackupIO backupHandler = new JSONQuestionBackupIO();
+        ArrayList<QuestionCreator> questionCreators = new ArrayList<>();
+
+        // Implementar estructura para QuestionCreator
+
+        Model model = new Model(repository, backupHandler, questionCreators); // Settear para parámetro de QuestionCreator -> ¿Por qué un ArrayList?
+        BaseView view = new InteractiveView(); // ¿Abstraer más para nuevas posibles vistas?
+
+        Controller controller = new Controller(model, view);
+
+        // Iniciar app
+        controller.load();
+
+        controller.init();
+    }
 }
